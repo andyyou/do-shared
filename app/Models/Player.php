3725b6ad69812b4e73013732d76e6b6713450fcf
andyyou <andyyu0920@gmail.com>
2025-07-11 11:49:22 +0800

    開發者A: 在Player model加入phone欄位

--- app/Models/Player.php
+++ app/Models/Player.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Player extends Model
 {
-    //
+    public $incrementing = false;
+
+    protected $keyType = 'string';
+
+    protected $fillable = [
+        'phone',
+    ];
 }
